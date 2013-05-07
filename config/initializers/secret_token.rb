88ecdb6f894a3a3c93d6b0dcb35d4c4057756c27
Ruben Hernando <rhernando@informa.es>
2013-05-07 14:52:07 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Audirt::Application.config.secret_token = 'd52ae13f93378489b6688d93e153f0fa146bc628978a73fc30ed82f6292184c1cbb0bb8dcdd0c18bdd0866422320b8f1e2ec5f921d05ccf688ae762ab616d040'
